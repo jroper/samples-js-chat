06f2a6e4017ccc84b2db426b94bf5c573ced0ed6
James Roper <james@jazzy.id.au>
2019-08-27 14:08:37 +1000

    Minimalised presence.js

--- presence/presence.js
+++ presence/presence.js
@@ -22,68 +22,8 @@ const entity = new crdt.Crdt(
   "cloudstate.samples.chat.presence.Presence"
 );
 
-entity.commandHandlers = {
-  Connect: connect,
-  Monitor: monitor
-};
-
-/**
- * Connect a user, to make their presence active.
- *
- * This is a streamed call. As long as a user (id given by the entity id) is connected
- * to it, they are considered to be online.
- *
- * Here we use a Vote CRDT, which if at least one node votes is true, will be true.
- * So when the user connects, we invoke the connect() method (which we have defined
- * by enriching the CRDT in onStateSet), which will manage our vote accordingly.
- *
- * When they disconnect, the onStreamCancel callback is invoked, and we disconnect,
- * removing our vote if this is the last connection to this CRDT.
- */
-function connect(user, ctx) {
-  if (ctx.streamed) {
-    ctx.onStreamCancel = state => {
-      state.disconnect();
-    };
-    ctx.state.connect();
-  }
-}
-
-/**
- * User presence monitoring call.
- *
- * This is a streamed call. We add a onStateChange callback, so that whenever the CRDT
- * changes, if the online status has changed since the last message we pushed, we push
- * it.
- */
-function monitor(user, ctx) {
-  let online = ctx.state.atLeastOne;
-  if (ctx.streamed) {
-    ctx.onStateChange = state => {
-      if (online !== state.atLeastOne) {
-        online = state.atLeastOne;
-        return {online};
-      }
-    };
-  }
-  return {online};
-}
-
-/**
- * This avoids having to do a null check on the state in each of the commands,
- * instead when there is no state, this will be invoked instead.
- */
 entity.defaultValue = () => new crdt.Vote();
 
-/**
- * This is invoked whenever a new state is created, either by the default value
- * handler above, or when the server pushes a new state. This is provided to allow
- * us to configure the CRDT, or enrich it with additional non replicated state, in this
- * case, for the vote CRDT, we add the number of users connected to this node to it,
- * so that only remove our vote when that number goes down to zero, along with
- * connect/disconnect functions for managing the users and updating the vote
- * accordingly.
- */
 entity.onStateSet = state => {
   state.users = 0;
   // Enrich the state with callbacks for users connected
@@ -101,5 +41,27 @@ entity.onStateSet = state => {
   };
 };
 
-// Export the entity
+function connect(user, ctx) {
+  ctx.onStreamCancel = state => {
+    state.disconnect();
+  };
+  ctx.state.connect();
+}
+
+function monitor(user, ctx) {
+  let online = ctx.state.atLeastOne;
+  ctx.onStateChange = state => {
+    if (online !== state.atLeastOne) {
+      online = state.atLeastOne;
+      return {online};
+    }
+  };
+  return {online};
+}
+
+entity.commandHandlers = {
+  Connect: connect,
+  Monitor: monitor
+};
+
 module.exports = entity;
